3f69108442627bc5093d2c739692ef61f552ac95
JaAn24 <vargasjaime0224@hotmail.com>
2024-11-28 01:25:26 -0500

    Style: Progress Responsive Dashboard

--- Ubank/src/pages/Dashboard/Dashboard.tsx
+++ Ubank/src/pages/Dashboard/Dashboard.tsx
@@ -92,13 +92,14 @@ console.log(minorExpenses);
         <div className='body-content'>
           <CurrencyConverter totalBalance={TotalIncomes} savings={100000} minorExpenses={TotalMinorExpenses} />
             <div className='dash-progress-charts'>
-              <IncomeExpenseChart datachart={datachart.slice(1)}/>
-              <ProgressCard 
+            <ProgressCard 
                   title="Your saving goal" 
                   description="New pair of shoes 👠👠" 
                   currentAmount={100000} 
                   goalAmount={200000} 
                 />
+              <IncomeExpenseChart datachart={datachart.slice(1)}/>
+              
             </div>
           </div>
         </section>
