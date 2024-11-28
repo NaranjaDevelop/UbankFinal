004615ace2fc73667f5c283d02bf4a76543e6103
JaAn24 <vargasjaime0224@hotmail.com>
2024-11-28 00:44:35 -0500

    Style: Responsive Dashboard

--- Ubank/src/pages/Dashboard/Dashboard.tsx
+++ Ubank/src/pages/Dashboard/Dashboard.tsx
@@ -83,7 +83,7 @@ console.log(minorExpenses);
       
   return (
     <div className="dashboard-content">
-      <Sidebar onLogout={handleLogout} />
+      <Sidebar className="sidebar" onLogout={handleLogout} />
       <section className='info-section'>
         <div className='text-content'>
         <h2>Welcome!, {username}</h2>
